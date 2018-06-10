5b4dbe505a4cefab1957b49d71f1dfe579643592
unknown <ritabratamaiti@gmail.com>
2018-06-10 15:03:41 +0800

    Signed-off-by: unknown <ritabratamaiti@gmail.com>

---
+++ RapidML/__init__.py
@@ -0,0 +1,255 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 10 10:58:51 2018
+
+@author: Ritabrata Maiti
+"""
+import pandas as pd   
+from sklearn.preprocessing import LabelEncoder
+from collections import defaultdict
+import dill
+from tpot import TPOTClassifier
+from tpot import TPOTRegressor
+import os
+
+d = defaultdict(LabelEncoder)
+X = []
+Y = []
+
+def rapid_classifier(df,g=5,p=50,v=2):
+    #Labelencoding the table
+    
+    fit = df.apply(lambda x: d[x.name].fit_transform(x))
+    df = fit.values
+    
+    #getting X and Y, for training the classifier
+    X = df[:, :(df.shape[1]-1)]
+    Y = df[:, df.shape[1]-1]
+    
+    clf = TPOTClassifier(generations=5, population_size=50, verbosity=2)
+    
+    newpath = r'webml' 
+    if not os.path.exists(newpath):
+        os.makedirs(newpath)
+    
+    #pickling the dictionary d
+    dill_file = open("webml/d", "wb")
+    dill_file.write(dill.dumps(d))
+    dill_file.close()
+    
+    #pickling the skeletal dataframe df_empty
+    dill_file = open("webml/df", "wb")
+    dill_file.write(dill.dumps(df_empty))
+    dill_file.close()
+    
+    #training and pickling the classifier
+    clf.fit(X, Y)
+    dill_file = open("webml/clf", "wb") 
+    dill_file.write(dill.dumps(clf.fitted_pipeline_))
+    dill_file.close()
+    
+    dill_file = open("webml/f", "wb")
+    print(','.join(str(e) for e in df2[2]))
+    dill_file.write(dill.dumps(','.join(str(e) for e in df2[2])))
+    dill_file.close()
+    
+    str1 = '''
+    #RD_AML created by Ritabrata Maiti
+    #Version: 1.0.0
+    
+    from flask import Flask, request
+    import dill
+    import helper
+    
+    
+    app = Flask(__name__)
+    app.config["DEBUG"] = True
+    
+    
+    @app.route('/')
+    def home():
+      return "RD_AML, Version: 1.0.0"
+    
+    
+    @app.route('/query', methods=['GET', 'POST'])
+    def query_example():
+        req = request.args['ip']
+        dill_file = open("f", "rb")
+        f = dill.load(dill_file)
+        dill_file.close()
+        l = [int(e) if e.isdigit() else e for e in f.split(',')]
+        req = req + ',' + str(l[-1])
+        dill_file = open("f", "wb")
+        dill_file.write(dill.dumps(req))
+        dill_file.close()
+        helper.predictor()
+        file = open('result.txt','r') 
+        res = file.read()
+        file.close()
+        return res
+    
+    if __name__ == '__main__':
+        app.run(debug=True, port=5000)
+    '''
+    
+    file = open("webml/API.py", "w")
+    file.write(str1)
+    file.close()
+    
+    str1 = '''
+    #RD_AML created by Ritabrata Maiti
+    #Version: 1.0.0
+    
+    import dill
+    import pandas as pd  
+    
+    def predictor():
+     
+        def fopen(str1, str2):
+            dill_file = open(str1, str2)
+            d = dill.load(dill_file)
+            dill_file.close()
+            return d
+        
+        d = fopen("d", "rb")
+        df = fopen("df", "rb")
+        f = fopen("f", "rb")
+        clf = fopen("clf", "rb")
+        l = [int(e) if e.isdigit() else e for e in f.split(',')]
+        df.loc[0] = l
+        fit = df.apply(lambda x: d[x.name].transform(x))
+        df1 = fit.values
+        X = df1[:, :(df1.shape[1]-1)]
+        p = clf.predict(X)
+        p = d[list(df)[-1]].inverse_transform(p)
+        file = open('result.txt','w') 
+        file.write(str(p[0]))
+        file.close()
+        return 0
+    
+    predictor()
+    '''
+    
+    file = open("webml/helper.py", "w")
+    file.write(str1)
+    file.close()
+
+
+def rapid_regressor(df,g=5,p=50,v=2):
+    #Labelencoding the table
+    
+    fit = df.apply(lambda x: d[x.name].fit_transform(x))
+    df = fit.values
+    
+    #getting X and Y, for training the classifier
+    X = df[:, :(df.shape[1]-1)]
+    Y = df[:, df.shape[1]-1]
+    
+    clf = TPOTRegressor(generations=5, population_size=50, verbosity=2)
+    
+    newpath = r'webml' 
+    if not os.path.exists(newpath):
+        os.makedirs(newpath)
+    
+    #pickling the dictionary d
+    dill_file = open("webml/d", "wb")
+    dill_file.write(dill.dumps(d))
+    dill_file.close()
+    
+    #pickling the skeletal dataframe df_empty
+    dill_file = open("webml/df", "wb")
+    dill_file.write(dill.dumps(df_empty))
+    dill_file.close()
+    
+    #training and pickling the classifier
+    clf.fit(X, Y)
+    dill_file = open("webml/clf", "wb") 
+    dill_file.write(dill.dumps(clf.fitted_pipeline_))
+    dill_file.close()
+    
+    dill_file = open("webml/f", "wb")
+    print(','.join(str(e) for e in df2[2]))
+    dill_file.write(dill.dumps(','.join(str(e) for e in df2[2])))
+    dill_file.close()
+    
+    str1 = '''
+    #RD_AML created by Ritabrata Maiti
+    #Version: 1.0.0
+    
+    from flask import Flask, request
+    import dill
+    import helper
+    
+    
+    app = Flask(__name__)
+    app.config["DEBUG"] = True
+    
+    
+    @app.route('/')
+    def home():
+      return "RD_AML, Version: 1.0.0"
+    
+    
+    @app.route('/query', methods=['GET', 'POST'])
+    def query_example():
+        req = request.args['ip']
+        dill_file = open("f", "rb")
+        f = dill.load(dill_file)
+        dill_file.close()
+        l = [int(e) if e.isdigit() else e for e in f.split(',')]
+        req = req + ',' + str(l[-1])
+        dill_file = open("f", "wb")
+        dill_file.write(dill.dumps(req))
+        dill_file.close()
+        helper.predictor()
+        file = open('result.txt','r') 
+        res = file.read()
+        file.close()
+        return res
+    
+    if __name__ == '__main__':
+        app.run(debug=True, port=5000)
+    '''
+    
+    file = open("webml/API.py", "w")
+    file.write(str1)
+    file.close()
+    
+    str1 = '''
+    #RD_AML created by Ritabrata Maiti
+    #Version: 1.0.0
+    
+    import dill
+    import pandas as pd  
+    
+    def predictor():
+     
+        def fopen(str1, str2):
+            dill_file = open(str1, str2)
+            d = dill.load(dill_file)
+            dill_file.close()
+            return d
+        
+        d = fopen("d", "rb")
+        df = fopen("df", "rb")
+        f = fopen("f", "rb")
+        clf = fopen("clf", "rb")
+        l = [int(e) if e.isdigit() else e for e in f.split(',')]
+        df.loc[0] = l
+        fit = df.apply(lambda x: d[x.name].transform(x))
+        df1 = fit.values
+        X = df1[:, :(df1.shape[1]-1)]
+        p = clf.predict(X)
+        p = d[list(df)[-1]].inverse_transform(p)
+        file = open('result.txt','w') 
+        file.write(str(p[0]))
+        file.close()
+        return 0
+    
+    predictor()
+    '''
+    
+    file = open("webml/helper.py", "w")
+    file.write(str1)
+    file.close()
+    
---
+++ setup.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 10 11:23:02 2018
+
+@author: Ritabrata Maiti
+"""
+
+from setuptools import setup
+
+setup(name='RapidML',
+      version='0.1',
+      description='Automated Machine Learning Model Creation, with corresponding model access via cloud through API',
+      url='',
+      author='Ritabrata Maiti',
+      author_email='ritabratamaiti@hiretrex.com',
+      license='',
+      packages=['RapidML'],
+      zip_safe=False,
+    install_requires=['numpy>=1.12.1',
+                    'scipy>=0.19.0',
+                    'scikit-learn>=0.18.1',
+                    'deap>=1.0',
+                    'update_checker>=0.16',
+                    'tqdm>=4.11.2',
+                    'stopit>=1.1.1',
+                    'pandas>=0.20.2',
+                    'TPOT>=0.9'],
+        extras_require={
+        'xgboost': ['xgboost==0.6a2'],
+        'skrebate': ['skrebate>=0.3.4'],
+        'mdr': ['scikit-mdr>=0.4.4']
+    },
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence'
+    ],
+    keywords=['Machine Learning API','pipeline optimization', 'hyperparameter optimization', 'data science', 'machine learning', 'genetic programming', 'evolutionary computation'],
+)
